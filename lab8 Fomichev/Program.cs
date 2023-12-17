96c7d1bd4632c9f939e921af47a2cdbf4a174b8d
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-17 22:08:50 +0200

    Добавьте файлы проекта.

---
+++ lab8 Fomichev/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int hours, minutes, seconds, milliseconds;
+        Console.WriteLine("Введите часы:");
+        hours = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите минуты:");
+        minutes = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите секунды:");
+        seconds = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите миллисекунды:");
+        milliseconds = int.Parse(Console.ReadLine());
+        TimeSpan time = new TimeSpan(hours, minutes, seconds, milliseconds);
+        Console.WriteLine($"Объединенное время: {time}");
+        Console.WriteLine("Введите год:");
+        int year = int.Parse(Console.ReadLine());
+
+        if (DateTime.IsLeapYear(year))
+        {
+            Console.WriteLine("Введенный год - високосный.");
+        }
+        else
+        {
+            int nextLeapYear = year;
+            while (!DateTime.IsLeapYear(nextLeapYear))
+            {
+                nextLeapYear++;
+            }
+            int yearsUntilNextLeapYear = nextLeapYear - year;
+            Console.WriteLine($"До ближайшего високосного года осталось {yearsUntilNextLeapYear} лет.");
+        }
+    }
+}
